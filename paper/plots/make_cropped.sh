4c291bae786daf03568d8a1220235baf1aabc81d
Jason Yosinski <jason@yosinski.com>
2014-06-06 14:39:20 -0400

    Added a fig

--- paper/plots/make_cropped.sh
+++ paper/plots/make_cropped.sh
@@ -2,3 +2,4 @@
 
 pdfcrop result_transfer_lines.pdf result_transfer_lines_crop.pdf
 pdfcrop result_transfer.pdf result_transfer_crop.pdf
+pdfcrop result_random_nm_combined.pdf result_random_nm_combined_crop.pdf
